8d30c975b9f1655f2457b67d2e6edf32e3c94377
Susanto <sushantomahato1@gmail.com>
2023-12-17 23:14:22 +0530

    initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'spaced-comment': 'off',
+    'no-console': 'warn',
+    'consistent-return': 'warn',
+    'func-names': 'off',
+    'object-shorthand': 'off',
+    'no-process-exit': 'off',
+    'no-param-reassign': 'off',
+    'no-return-await': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-destructuring': ['error', { object: true, array: false }],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'req|res|next|val' }],
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="flex justify-between">
+      <div></div>
+      <div className="w-[326px] bg-[#2D2D2D;] h-screen"></div>
+    </div>
+  );
+}
+
+export default App;
