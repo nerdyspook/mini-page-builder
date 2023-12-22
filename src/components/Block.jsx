597a86fbb03a244b38ffc5453a4390b5b931e5ec
Susanto <sushantomahato1@gmail.com>
2023-12-22 16:57:44 +0530

    fix: selected element type block gets darken

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,6 +1,6 @@
 import Grip from './../assets/grip-vertical.svg?react';
 
-const Block = ({ elementType, setSelectedElement }) => {
+const Block = ({ elementType, setSelectedElement, selectedElement }) => {
   const handleDragStart = (event, type) => {
     event.dataTransfer.setData('blockElement', type); // adding metadata to event
   };
@@ -20,7 +20,11 @@ const Block = ({ elementType, setSelectedElement }) => {
 
   return (
     <li
-      className="flex items-center py-2 md:py-4 rounded mb-2 bg-white w-full cursor-grab"
+      className={`flex items-center py-2 md:py-4 rounded mb-2  w-full cursor-grab ${
+        selectedElement.type === elementType.toLowerCase()
+          ? 'bg-slate-300'
+          : 'bg-white'
+      }`}
       draggable
       onTouchStart={handleTouchStart}
       onDragStart={(e) => handleDragStart(e, elementType.toLowerCase())}
